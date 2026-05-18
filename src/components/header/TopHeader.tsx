8bb7de1bd9c94e9c6dfec85fff8b2026220e1c3e
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-05-19 00:17:00 +0600

    Header

--- src/components/header/TopHeader.tsx
+++ src/components/header/TopHeader.tsx
@@ -6,12 +6,14 @@ import { logoShoplix } from '@/assets'
 
 const TopHeader = () => {
   return (
-    <div className='border-b border-b-gray-400'>TopHeader
-    <Container >
+    <div className='border-b border-b-gray-400'>
+    <Container className='py-4 flex items-center gap-4 md:gap-6 lg:gap-20 justify-between'>
       <Image src={logoShoplix} alt='Logo' className='h-12 w-36'/>
+      <div>Searchbar</div>
+      <div>Navigation Menu</div>
     </Container>
     </div>
   )
-}
+} 
 
 export default TopHeader
