37dd5116f9b2f8889b9cd14c8b08ff88783cdbf2
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-06-14 00:38:12 +0600

    not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,27 @@
-import Container from '@/components/Container'
-import Image from 'next/image'
-import React from 'react'
+import Container from "@/components/Container";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 const NotFound = () => {
   return (
-    <div className='max-w-7xl mx-auto'>
-      <Container>
-      <Image src="/images/notFoundPage.png" width={500} height={600} alt='notFound'/>
-    </Container>
+    <div className="max-w-7xl  mx-auto ">
+      <Container className="flex flex-col items-center justify-center text-center ">
+        <Image
+          src="/images/notFoundPage.png"
+          width={400}
+          height={400}
+          alt="notFound"
+        />
+        <Link href="/" className=" items-center justify-center">
+          <button className=" flex text-white text-lg items-center justify-center border border-cyan-600 bg-cyan-600 hover:bg-cyan-700 py-1 px-4 rounded-md cursor-pointer">
+            <IoIosArrowRoundBack /> Back to Home
+          </button>
+        </Link>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
