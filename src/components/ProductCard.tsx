1ea52c26175a5d80dbba007dacf0d66fbc6c3cdd
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-06-19 01:27:40 +0600

    product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,12 +1,22 @@
-import { ProductType } from "../../typs"
+import Link from "next/link";
+import { ProductType } from "../../typs";
+import Image from "next/image";
 
-
-const ProductCard = ({product}: {product: ProductType}) => {
+const ProductCard = ({ product }: { product: ProductType }) => {
   return (
     <div className=" border border-gray-400 hover:shadow-lg hover:shadow-black/30 duration-200 rounded-md group overflow-hidden relative">
-      ProductType
+      {/* image */}
+      <Link href={"/products"}>
+        <Image src={product?.images[0]} alt="Product-Image" 
+        width={500}
+        height={500}
+        priority={true}
+        className=" w-full h-64 object-contain hover:scale-110 duration-300"
+        />
+        <p className=" absolute top-2 right-2 text-white bg-orange-500 py-1 px-2 text-xs rounded-lg">{product?.discountPercentage}%</p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
