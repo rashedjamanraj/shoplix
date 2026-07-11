3e6e54e64163886c9a5668d23d3768bcea109c5c
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-07-12 00:31:08 +0600

    add view cart signin/signout

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
+// import FacebookProvider from "next-auth/providers/facebook"
 
 
  const authOptions = {
@@ -15,10 +15,10 @@ import FacebookProvider from "next-auth/providers/facebook"
   clientId: process.env.GOOGLE_CLIENT_ID!,
   clientSecret: process.env.GOOGLE_SECRET!,
 }),
-FacebookProvider({
-  clientId: process.env.FACEBOOK_CLIENT_ID!,
-  clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-}),
+// FacebookProvider({
+//   clientId: process.env.FACEBOOK_CLIENT_ID!,
+//   clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+// }),
   ],
 };
 
